5fd18f79e57b2fc09b1c4101705b78b76ea00a1d
LAPTOP-AM02MI6O\Syahbarudin Abdillah <syahbarabdillah@gmail.com>
2024-03-21 16:43:33 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.shop.nothing"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.shop.nothing"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.retrofit2:retrofit:2.6.4")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.4")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.chuckerteam.chucker:library:4.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.loopj.android:android-async-http:1.4.11")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/shop/nothing/model/ResponseUser.kt
@@ -0,0 +1,15 @@
+package com.shop.nothing.model
+import com.google.gson.annotations.SerializedName
+
+class ResponseUser {
+    @field:SerializedName("page")
+    val page: Int? = null
+    @field:SerializedName("per_page")
+    val perPage: Int? = null
+    @field:SerializedName("total")
+    val total: Int? = null
+    @field:SerializedName("total_pages")
+    val totalPages: Int? = null
+    @field:SerializedName("data")
+    val data: List<DataItem>? = null
+}
